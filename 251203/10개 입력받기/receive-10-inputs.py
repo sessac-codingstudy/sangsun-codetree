b210ef6d8fdcb557efcdee9c7abf215de496d544
b.upline_00 <sano820@naver.com>
2025-12-03 19:06:21 +0900

    Title: 10개 입력받기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251203/10개 입력받기/receive-10-inputs.py
@@ -0,0 +1,12 @@
+nums = list(map(int, input().split()))
+
+valid_nums = []
+for n in nums:
+    if n == 0:
+        break
+    valid_nums.append(n)
+
+a = sum(valid_nums)
+b = a / len(valid_nums)
+
+print(a, round(b,1))
